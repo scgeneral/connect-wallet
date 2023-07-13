33cb9b65e41aff18fab129b548b5c53af8a11ef3
alvitsit <al.tselischev@gmail.com>
2023-07-13 12:23:01 +0400

    fix: interface

--- src/interface.ts
+++ src/interface.ts
@@ -1,11 +1,5 @@
 import { Contract } from 'web3-eth-contract';
 import { AbiItem } from 'web3-utils';
-import { MetamaskConnect } from 'metamask';
-import { WalletsConnect } from 'wallet-connect';
-import { CoinbaseWalletConnect } from 'coinbase-wallet';
-import { KardiaChainConnect } from 'kardiachain';
-import { OntoConnect } from 'onto';
-import { GameStopConnect } from 'gamestop';
 
 export interface IProvider {
     name: string;
@@ -131,14 +125,6 @@ export interface IChain {
     chainID: number;
     hex: string;
 }
-
-export type IProviderConnect =
-    | MetamaskConnect
-    | WalletsConnect
-    | CoinbaseWalletConnect
-    | KardiaChainConnect
-    | OntoConnect
-    | GameStopConnect;
 
 declare global {
     interface Window {
